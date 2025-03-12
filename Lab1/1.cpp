94ad41fed8753a91078576f7d3b47b374d32014d
stekl0 <greystekl0@gmail.com>
2025-03-12 14:47:18 +0500

    lab1.2

--- Lab1/1.cpp
+++ Lab1/1.cpp
@@ -1,68 +1,88 @@
 #include <iostream>
 #include <fstream>
 
-enum State {
+enum State
+{
     NORMAL,
     SLASH,
     MULTI_COMMENT,
     STAR_IN_COMMENT
 };
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
         std::cerr << "Usage: " << argv[0] << " <input file> <output file>" << std::endl;
         return 1;
     }
 
     std::ifstream in(argv[1], std::ios::binary);
-    if (!in) {
+    if (!in)
+    {
         std::cerr << "Could not open input file." << std::endl;
         return 1;
     }
 
     std::ofstream out(argv[2], std::ios::binary);
-    if (!out) {
+    if (!out)
+    {
         std::cerr << "Could not open output file." << std::endl;
         return 1;
     }
 
     State state = NORMAL;
     char c;
-    while (in.get(c)) {
-        switch (state) {
-            case NORMAL:
-                if (c == '/') {
-                    state = SLASH;
-                } else {
-                    out.put(c);
-                }
-                break;
-            case SLASH:
-                if (c == '*') {
-                    state = MULTI_COMMENT;
-                } else if (c == '/') {
-                    out.put('/');
-                } else {
-                    out.put('/');
-                    out.put(c);
-                    state = NORMAL;
-                }
-                break;
-            case MULTI_COMMENT:
-                if (c == '*') {
-                    state = STAR_IN_COMMENT;
-                }
-                break;
-            case STAR_IN_COMMENT:
-                if (c == '/') {
-                    state = NORMAL;
-                } else if (c != '*') {
-                    state = MULTI_COMMENT;
-                }
-                break;
+    while (in.get(c))
+    {
+        switch (state)
+        {
+        case NORMAL:
+            if (c == '/')
+            {
+                state = SLASH;
+            }
+            else
+            {
+                out.put(c);
+            }
+            break;
+        case SLASH:
+            if (c == '*')
+            {
+                state = MULTI_COMMENT;
+            }
+            else if (c == '/')
+            {
+                out.put('/');
+            }
+            else
+            {
+                out.put('/');
+                out.put(c);
+                state = NORMAL;
+            }
+            break;
+        case MULTI_COMMENT:
+            if (c == '*')
+            {
+                state = STAR_IN_COMMENT;
+            }
+            break;
+        case STAR_IN_COMMENT:
+            if (c == '/')
+            {
+                state = NORMAL;
+            }
+            else if (c != '*')
+            {
+                state = MULTI_COMMENT;
+            }
+            break;
         }
     }
-    if (state == SLASH) {
+    if (state == SLASH)
+    {
         out.put('/');
     }
 
--- Lab1/2.cpp
+++ Lab1/2.cpp
@@ -1,137 +1,156 @@
 #include <iostream>
 #include <fstream>
 
-enum State {
-    NORMAL,         // обычное состояние – выводим символы в выходной поток
-    SLASH,          // только что прочитали '/', ожидаем следующий символ для определения комментария
-    COMMENT,        // внутри многострочного комментария
-    STAR,           // после '*' внутри многострочного комментария – проверяем, не конец ли комментария
-    SINGLE_COMMENT, // внутри однострочного комментария (начало с //)
-    IN_STRING,      // внутри строкового литерала (начинается с ")
-    IN_CHAR         // внутри символьной константы (начинается с ')
+enum State
+{
+    NORMAL,
+    SLASH,
+    MULTI_COMMENT,
+    STAR_IN_MULTI_COMMENT,
+    SINGLE_COMMENT,
+    IN_STRING,
+    IN_CHAR,
+    SLASH_IN_STRING,
+    SLASH_IN_CHAR
 };
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Использование: " << argv[0] << " <входной файл> <выходной файл>\n";
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <input file> <output file>" << std::endl;
         return 1;
     }
 
-    std::ifstream fin(argv[1], std::ios::in);
-    if (!fin) {
-        std::cerr << "Ошибка открытия входного файла: " << argv[1] << "\n";
+    std::ifstream in(argv[1], std::ios::binary);
+    if (!in)
+    {
+        std::cerr << "Could not open input file." << std::endl;
         return 1;
     }
 
-    std::ofstream fout(argv[2], std::ios::out);
-    if (!fout) {
-        std::cerr << "Ошибка открытия выходного файла: " << argv[2] << "\n";
-        fin.close();
+    std::ofstream out(argv[2], std::ios::binary);
+    if (!out)
+    {
+        std::cerr << "Could not open output file." << std::endl;
         return 1;
     }
 
     State state = NORMAL;
     char c;
 
-    while (fin.get(c)) {
-        switch (state) {
-            case NORMAL:
-                if (c == '/') {
-                    // При обнаружении '/' переходим в состояние SLASH, чтобы проверить, начинается ли комментарий
-                    state = SLASH;
-                } else if (c == '"') {
-                    // Начало строкового литерала
-                    fout.put(c);
-                    state = IN_STRING;
-                } else if (c == '\'') {
-                    // Начало символьной константы
-                    fout.put(c);
-                    state = IN_CHAR;
-                } else {
-                    // Обычный символ – выводим его
-                    fout.put(c);
-                }
-                break;
+    while (in.get(c))
+    {
+        switch (state)
+        {
+        case NORMAL:
+            if (c == '/')
+            {
+                state = SLASH;
+            }
+            else if (c == '"')
+            {
+                out.put(c);
+                state = IN_STRING;
+            }
+            else if (c == '\'')
+            {
+                out.put(c);
+                state = IN_CHAR;
+            }
+            else
+            {
+                out.put(c);
+            }
+            break;
 
-            case SLASH:
-                if (c == '*') {
-                    // Начинается многострочный комментарий
-                    state = COMMENT;
-                } else if (c == '/') {
-                    // Начинается однострочный комментарий
-                    state = SINGLE_COMMENT;
-                } else {
-                    // Это не комментарий: выводим ранее прочитанный '/' и текущий символ
-                    fout.put('/');
-                    fout.put(c);
-                    state = NORMAL;
-                }
-                break;
+        case SLASH:
+            if (c == '*')
+            {
+                state = MULTI_COMMENT;
+            }
+            else if (c == '/')
+            {
+                state = SINGLE_COMMENT;
+            }
+            else
+            {
+                out.put('/');
+                out.put(c);
+                state = NORMAL;
+            }
+            break;
 
-            case COMMENT:
-                if (c == '*') {
-                    // Возможно, конец многострочного комментария – переходим в состояние STAR
-                    state = STAR;
-                }
-                // Иначе символы игнорируются
-                break;
+        case MULTI_COMMENT:
+            if (c == '*')
+            {
+                state = STAR_IN_MULTI_COMMENT;
+            }
+            break;
 
-            case STAR:
-                if (c == '/') {
-                    // Найден конец многострочного комментария, возвращаемся в NORMAL
-                    state = NORMAL;
-                } else if (c != '*') {
-                    // Если встретили не '*', возвращаемся в состояние COMMENT
-                    state = COMMENT;
-                }
-                // Если c == '*', остаёмся в состоянии STAR
-                break;
+        case STAR_IN_MULTI_COMMENT:
+            if (c == '/')
+            {
+                out.put(' ');
+                state = NORMAL;
+            }
+            else if (c != '*')
+            {
+                state = MULTI_COMMENT;
+            }
+            break;
 
-            case SINGLE_COMMENT:
-                // Пропускаем символы однострочного комментария до символа перевода строки
-                if (c == '\n' || c == '\r') {
-                    fout.put(c);
-                    state = NORMAL;
-                }
-                break;
+        case SINGLE_COMMENT:
+            if (c == '\n' || c == '\r')
+            {
+                out.put(c);
+                state = NORMAL;
+            }
+            break;
 
-            case IN_STRING:
-                // Выводим все символы внутри строкового литерала
-                fout.put(c);
-                if (c == '\\') {
-                    // Экранирование – выводим следующий символ без проверки
-                    if (fin.get(c)) {
-                        fout.put(c);
-                    }
-                } else if (c == '"') {
-                    // Завершение строкового литерала
-                    state = NORMAL;
-                }
-                break;
+        case IN_STRING:
+            out.put(c);
+            if (c == '\\')
+            {
+                state = SLASH_IN_STRING;
+            }
+            else if (c == '"')
+            {
+                state = NORMAL;
+            }
+            break;
 
-            case IN_CHAR:
-                // Выводим символы внутри символьной константы
-                fout.put(c);
-                if (c == '\\') {
-                    // Экранирование – выводим следующий символ
-                    if (fin.get(c)) {
-                        fout.put(c);
-                    }
-                } else if (c == '\'') {
-                    // Завершение символьной константы
-                    state = NORMAL;
-                }
-                break;
+        case IN_CHAR:
+            out.put(c);
+            if (c == '\\')
+            {
+                state = SLASH_IN_CHAR;
+            }
+            else if (c == '\'')
+            {
+                state = NORMAL;
+            }
+            break;
+
+        case SLASH_IN_STRING:
+            out.put(c);
+            state = IN_STRING;
+            break;
+
+        case SLASH_IN_CHAR:
+            out.put(c);
+            state = IN_CHAR;
+            break;
         }
     }
 
-    // Если файл закончился сразу после символа '/', выводим его
-    if (state == SLASH) {
-        fout.put('/');
+    if (state == SLASH)
+    {
+        out.put('/');
     }
 
-    fin.close();
-    fout.close();
+    in.close();
+    out.close();
 
     return 0;
 }
